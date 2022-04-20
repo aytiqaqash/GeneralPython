af79ebb6ae3a1d99d2694d7400ba5b9b982aa970
aytiqaqash <aytiqaqash@gmail.com>
2022-04-20 00:31:53 -0400

    Insertion Sort Alqorithm

---
+++ InsertionSort.py
@@ -0,0 +1,24 @@
+def insertion_sort(list):
+    for i in range(1, len(list)):
+        print("addım: " + str(i))
+        key = list[i]
+        j = i - 1
+
+        while j >= 0 and key < list[j]:
+            print(str(j) + " sıfırdan çoxdur bərabərdir və " + str(key) + " götürdüyümüz element " + str(
+                list[j]) + "dən azdır?")
+            print("bəli, yerdəyişmə edirik: " + str(list[j + 1]) + " və " + str(list[j]))
+            print(list)
+            list[j + 1] = list[j]
+            j -= 1
+            list[j + 1] = key
+            print(list)
+
+    print()
+    print("SON NƏTİCƏ: ")
+    return list
+
+
+not_sorted_list = [12, 5, 3, 1, 8, 7, 2, 4]
+
+print(insertion_sort(not_sorted_list))
