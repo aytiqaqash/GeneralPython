3b143775aa2714644fdb01b143476fdefe5e3833
aytiqaqash <aytiqaqash@gmail.com>
2022-05-25 00:52:18 -0400

    add comments

--- CheckForDuplicates.py
+++ CheckForDuplicates.py
@@ -1,8 +1,17 @@
+# Input:  [1, 2, 1, 3, 2]
+# Output:
+# 1 - not mentioned
+# 2 - not mentioned
+# 1 - duplicate
+# 3 - not mentioned
+# 2 - duplicate
+
+
 list = [1, 2, 1, 3, 2]
 newList = []
 for i in list:
   if i in newList:
-    print(str(i) + " - dubl")
+    print(str(i) + " - duplicate")
   else:
     newList.append(i)
-    print(str(i) + " -  не встречалось ")
+    print(str(i) + " - not mentioned")
