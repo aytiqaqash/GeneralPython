dbb00c45dadf0a726c577433624548573c1bfc57
aytiqaqash <aytiqaqash@gmail.com>
2022-07-25 14:32:33 -0400

    Task 2 added

---
+++ DictionariesTasks/hw-3.2.py
@@ -0,0 +1,6 @@
+# 2. for dongusu yazin ki dictionarydeki butun listleri bosh bir liste daxil etsin
+dictionary = {'name':{'oglan_adlari':['Nizami','Elsen','Ulvi'],'qiz_adlari':['Aygun','Sehla','Ulker']},
+          'soyad':{'oglanlarin_soyadlari':['Aliyev','Quliyev','Haciyev'],'qizlarin_soyadlari':['Rehmanova','Quliyeva','Ismayilova']},
+          'tehsil':{'oglanlarin_tehsili':['ali','orta','ali'],'qizlarin_tehsili':['ali','ali','ali']},
+          'yas':{'oglanlarin_yasi':[29,32,34],'qizlarin_yasi':[24,26,27]}}
+# Numune ==> a = ['Nizami',Elsen',Ulvi','Aygun','Sehla','Ulker','Aliyev','Quliyev','Haciyev','Rehmanova','Quliyeva','Ismayilova','ali','orta','ali']
