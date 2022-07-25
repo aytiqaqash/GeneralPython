54f9e67694cdf693113c756e8d75b5c9edd8191c
aytiqaqash <aytiqaqash@gmail.com>
2022-07-25 14:37:44 -0400

    Tasks 3.3, 3.4, 3.5 were done

--- DictionariesTasks/hw-3.3.py
+++ DictionariesTasks/hw-3.3.py
@@ -4,4 +4,14 @@ dictionary = {'name':{'oglan_adlari':['Nizami','Elsen','Ulvi'],'qiz_adlari':['Ay
           'tehsil':{'oglanlarin_tehsili':['ali','orta','ali'],'qizlarin_tehsili':['ali','ali','ali']},
           'yas':{'oglanlarin_yasi':[29,32,34],'qizlarin_yasi':[24,26,27]}}
 
+for k, v in dictionary.items():
+  if type(v) == list and len(v)>1:
+    for i in v:
+          v.remove(i)
+  else:
+    for nv in v.values():
+      if type(nv) == list and len(nv)>1:
+        for i in nv:
+          nv.remove(i)
 
+print(dictionary)
--- DictionariesTasks/hw-3.4.py
+++ DictionariesTasks/hw-3.4.py
@@ -4,3 +4,35 @@ dictionary = {'name':{'oglan_adlari':['Nizami','Elsen','Ulvi'],'qiz_adlari':['Ay
           'tehsil':{'oglanlarin_tehsili':['ali','orta','ali'],'qizlarin_tehsili':['ali','ali','ali']},
           'yas':{'oglanlarin_yasi':[29,32,34],'qizlarin_yasi':[24,26,27]}}
 # Numune ==> 'Nizami Rehmanova'
+
+Name = []
+LName = []
+Education = []
+Age = []
+for key,vals in dictionary.items():
+  if key == 'name':
+    for x in vals.values():
+      for i in x:
+        Name.append(i)
+  elif key == 'soyad':
+    for x in vals.values():
+      for i in x:
+        LName.append(i)
+  elif key == 'tehsil':
+    for x in vals.values():
+      for i in x:
+        Education.append(i)
+  elif key == 'yas':
+    for x in vals.values():
+      for i in x:
+        Age.append(i)
+
+result = ''
+# LName = LName.sort(reverse=True) # bu niyə alınmadı? None yazırdı
+LName = sorted(LName,reverse=True)
+for i in range(len(Name)):
+  result = result + Name[i] + " " + LName[i]
+  if i < len(Name)-1:
+    result += ", "
+
+print(result)
