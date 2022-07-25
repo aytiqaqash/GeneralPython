07e965dfda7c2fe74880fcad8761bb936e36b054
aytiqaqash <aytiqaqash@gmail.com>
2022-07-25 14:33:07 -0400

    Task 2 is done

--- DictionariesTasks/hw-3.2.py
+++ DictionariesTasks/hw-3.2.py
@@ -4,3 +4,14 @@ dictionary = {'name':{'oglan_adlari':['Nizami','Elsen','Ulvi'],'qiz_adlari':['Ay
           'tehsil':{'oglanlarin_tehsili':['ali','orta','ali'],'qizlarin_tehsili':['ali','ali','ali']},
           'yas':{'oglanlarin_yasi':[29,32,34],'qizlarin_yasi':[24,26,27]}}
 # Numune ==> a = ['Nizami',Elsen',Ulvi','Aygun','Sehla','Ulker','Aliyev','Quliyev','Haciyev','Rehmanova','Quliyeva','Ismayilova','ali','orta','ali']
+
+all_lists = []
+for k, v in dictionary.items():
+  if type(v) == list:
+    for i in v:
+          all_lists.append(i)
+  else:
+    for nv in v.values():
+      if type(nv) == list:
+        for i in nv:
+          all_lists.append(i)
