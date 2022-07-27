d6c84c5be539540d45ac8457676ce4aaccb7e2a4
aytiqaqash <aytiqaqash@gmail.com>
2022-07-26 22:55:07 -0400

    səhv eləmişdim, məncə düzəltdim.

--- DictionariesTasks/hw-3.5.py
+++ DictionariesTasks/hw-3.5.py
@@ -8,23 +8,30 @@ liste = [{'name':{'oglan_adlari':['Nizami','Elsen','Ulvi'],'qiz_adlari':['Aygun'
           'tehsil':{'oglanlarin_tehsili':['ali','ali','ali'],'qizlarin_tehsili':['orta','ali','ali']},
           'yas':{'oglanlarin_yasi':[29,32,45],'qizlarin_yasi':[21,26,27]}}]
 # HINT: in,not in
-
 for lughet in liste[0]:
-  if lughet in liste[1]:
-    for item in liste[0][lughet]:
-      if item in liste[1][lughet]:
-        for val in liste[0][lughet][item]:
-          if val in liste[1][lughet][item]:
-            print(str(val) + " is in both dictionaries!")
-          else:
-            print(str(val) + " is only in the first dictionary!")
-  else:
-    for lughet2 in liste[1]:
-      if lughet2 not in liste[0]:
-        for item in liste[1][lughet2]:
-          if item in liste[0][lughet]:
-            for val in liste[1][lughet2][item]:
-              if val in liste[0][lughet][item]:
-                print(str(val) + " is in both dictionaries!")
-              else:
-                print(str(val) + " is only in the second dictionary!")
+    if lughet in liste[1]:
+        for item in liste[0][lughet]:
+            if item in liste[1][lughet]:
+                for val in liste[0][lughet][item]:
+                    if val in liste[1][lughet][item]:
+                        print(str(val) + " is in both dictionaries!")
+                    else:
+                        print(str(val) + " is only in the first dictionary!")
+            else:
+                print(item + " is only in the first dictionary!")
+    else:
+        print(lughet + " is only in the first dictionary!")
+
+for lughet in liste[1]:
+    if lughet in liste[0]:
+        for item in liste[1][lughet]:
+            if item in liste[0][lughet]:
+                for val in liste[1][lughet][item]:
+                    if val in liste[0][lughet][item]:
+                        continue
+                    else:
+                        print(str(val) + " is only in the second dictionary!")
+            else:
+                print(item + " is only in the second dictionary!")
+    else:
+        print(lughet + " is only in the second dictionary!")
