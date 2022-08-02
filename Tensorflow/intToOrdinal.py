e7e16ddee0d653e8c04d784d065c7731c27ba2a5
aytiqaqash <aytiqaqash@gmail.com>
2022-08-01 22:51:40 -0400

    #Exercise 89: Convert an Integer to Its Ordinal Number

---
+++ Tensorflow/intToOrdinal.py
@@ -0,0 +1,34 @@
+#Exercise 89: Convert an Integer to Its Ordinal Number
+
+"""
+Words like first, second and third are referred to as ordinal numbers. In this exercise,
+you will write a function that takes an integer as its only parameter and returns a
+string containing the appropriate English ordinal number as its only result. Your
+function must handle the integers between 1 and 12 (inclusive). It should return an
+empty string if the function is called with an argument outside of this range. Include
+a main program that demonstrates your function by displaying each integer from 1
+to 12 and its ordinal number. Your main program should only run when your file has
+not been imported into another program.
+"""
+
+def intToOrdinal(num):
+  dictionary = {
+      1:"first",
+      2:"second",
+      3:"third",
+      4:"fourth",
+      5:"fifth",
+      6:"sixth",
+      7:"seventh",
+      8:"eightth",
+      9:"nineth",
+      10:"tenth",
+      11:"eleventh",
+      12:"twelveth"
+  }
+  if num in dictionary.keys():
+    return dictionary[num]
+  return ""
+
+
+print(intToOrdinal(7))
