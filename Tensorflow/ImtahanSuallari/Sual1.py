62779dc60bc02abce968b66e1e143e7e56b42efc
aytiqaqash <aytiqaqash@gmail.com>
2022-08-12 18:29:11 -0400

    Tapshiriqlar

---
+++ Tensorflow/ImtahanSuallari/Sual1.py
@@ -0,0 +1,7 @@
+# Sual1 -- 10 balliq
+liste = [1, 2, 3, 4, 5, 6]
+# 1.1 listin icindeki deyerleri toplaya bilen funksiya yazin
+# 1.2 listin icindeki funksiyalari bir birile vurub hasil cixarda bilen
+# 1.3 input verildikde listin icine elave ede bilecek,inputda 'remove' yazdiqda ilk indexdexdeki deyeri remove ede bilecek funksiya yazin
+# 1.4 ele funksiya  yazin ki listin icerisindeki cut edelerin ceminden tek ededlerin cemin cixsin
+# 1.5 ele funksiya yazin ki listin icerisindeki cut ededlerin hasilini tek ededlerin hasiline bolsun
